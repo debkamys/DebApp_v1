88d33ab4020676ad781259649e566753da239d36
debkamys <debra.kamys@gmail.com>
2021-12-04 18:32:59 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    var i = 0;
+	    foreach (var arg in args)
+	    {
+            	i = i + 1;
+		Console.WriteLine($"Argument number {i} is {arg}");
+	    }
+        }
+    }
+}
